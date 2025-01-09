30f44c26b6501ceba9f8025df526ddbbc730845d
unknown <prohrman@gmail.com>
2025-01-09 13:24:50 -0500

    broke out section heading components and added about

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import About from "@/components/about";
 import Divider from "@/components/divider";
 import Intro from "@/components/intro";
 
@@ -5,5 +6,6 @@ export default function Home() {
   return <main className="flex flex-col items-center px-4">
     <Intro />
     <Divider />
+    <About />
   </main>;
 }
--- components/divider.tsx
+++ components/divider.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion"
 
 export default function Divider() {
   return (
-    <motion.div className='bg-gray-200 my-24 h-14 w-1 rounded-full hidden sm:block'
+    <motion.div className='bg-gray-200 my-20 h-14 w-1 rounded-full hidden sm:block'
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.125 }}

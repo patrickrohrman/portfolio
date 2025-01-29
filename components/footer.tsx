5e5fe00bab4068524f1a29082b1bac92f5ea5d76
unknown <prohrman@gmail.com>
2025-01-29 15:30:26 -0500

    fix spacing in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className='mt-10 px-4 text-center text-gray-500 pb-10 -mt-10'>
+    <footer className='px-4 text-center text-gray-500 pb-10 -mt-10'>
         <small className='block mb-2 text-xs'>
             &copy; {new Date().getFullYear()} Patrick Rohrman. All rights reserved.
         </small>

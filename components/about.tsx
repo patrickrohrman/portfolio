41fb7f5419593d56a8b9d03fc7e29ae2a71a7140
unknown <prohrman@gmail.com>
2025-01-29 16:12:16 -0500

    tweak about me text

--- components/about.tsx
+++ components/about.tsx
@@ -20,18 +20,14 @@ export default function About() {
         About Me
       </SectionHeading>
       <p className='mb-3'>
-        I’m an IT professional and web developer with a passion for creating efficient, user-friendly, and accessible digital experiences.
-      </p>
+        I’m an IT professional and web developer passionate about creating efficient, user-friendly, and accessible digital experiences.</p>
       <p className='mb-3'>
-        I’ve worked with several frontend JavaScript and CSS frameworks and tech such as <strong>React, TypeScript, Next.js, Bootstrap, Tailwind CSS, and Foundation</strong>. I’m comfortable working with both <strong>Linux and Windows servers, MySQL, Apache, NGINX</strong>, as well as other server configurations. I have experience working with a number of CMSs such as <strong>WordPress, Statamic, and Drupal</strong>. My background in systems administration, SEO, database management, and UI/UX design allows me to build solutions that are both technically sound and user focused.
-      </p>
+        I've worked with several front-end frameworks like <strong>React, TypeScript, Next.js, Bootstrap, Tailwind CSS, and Foundation,</strong> while also managing <strong>Linux and Windows servers using Apache, NGINX, MySQL, and other backend software</strong>. I have experience with CMS platforms like <strong>WordPress, Statamic, and Drupal</strong>, along with expertise in systems administration, SEO, database management, and UI/UX design.</p>
       <p className='mb-3'>
-        With experience in <strong>cloud server management</strong> and numerous IT and web development certifications I bring a well-rounded approach to developing new and exciting projects. I also prioritize customer service and problem-solving, ensuring that every project meets both business needs and user expectations.
-      </p>
+        With a background in <strong>cloud server management</strong> and multiple IT and web development certifications, I bring a well-rounded approach to every project. I prioritize customer service and problem-solving, ensuring that my work meets both business needs and user expectations.</p>
       <p>
         <i>Whether it's front-end design, system administration, or optimizing for search engines, I thrive on turning ideas into functional, high-performing websites. Let’s connect and build something great!</i>
       </p>
-
     </motion.section>
   )
 }
